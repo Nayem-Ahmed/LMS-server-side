9d101ae07252bf4589efb8175e9af4b991da38b3
unknown <freelancernayem.me@gmail.comm>
2024-03-06 22:18:28 +0600

    crud->post,get

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         const addBookCollection = client.db('LibraryMS').collection('addbooks')
+        const borrowedBooksCollection = client.db('LibraryMS').collection('borrowed')
 
         // post(add BOOKS) books
         app.post('/addbooks', async (req, res) => {
@@ -36,15 +37,59 @@ async function run() {
             res.send(result)
         })
 
-        // Get all books
+        // Get addall books
         app.get('/addbooks', async (req, res) => {
             const result = await addBookCollection.find().toArray()
             res.send(result)
         })
-        // Get single products
+        // Get addbooks by perams
+        app.get('/addbooks/email/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const result = await addBookCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Get single addbooks
         app.get('/addbooks/:id', async (req, res) => {
-            const id = req.params.id
+            const id = req.params.id;
             const result = await addBookCollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
+        // app.put('/addbooks/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) }
+        //     const Option = { upsert: true }
+        //     const updateProduct = req.body
+        //     const Product = {
+        //         $set: {
+        //             bookImage: updateProduct.bookImage,
+        //             bookName: updateProduct.bookName,
+        //             quantity: updateProduct.quantity,
+        //             authorName: updateProduct.authorName,
+        //             category: updateProduct.category,
+        //             Rating: updateProduct.Rating,
+        //         }
+        //     }
+        //     const result = await addBookCollection.updateOne(filter, Product, Option)
+        //     res.send(result)
+
+        // })
+        // post borrowed books
+        app.post('/borrowed', async (req, res) => {
+            const borrowedbook = req.body;
+            const result = await borrowedBooksCollection.insertOne(borrowedbook)
+            res.send(result)
+        })
+        app.get('/borrowed', async (req, res) => {
+            const result = await borrowedBooksCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/borrowed/:email', async (req, res) => {
+            const email = req.params.email;
+            const fiter = { email: email }
+            const result = await borrowedBooksCollection.find(fiter).toArray()
             res.send(result)
         })
 
